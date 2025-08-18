0c0c29ea616c7971fa24ff2b5dae8ced24dd63bc
henrqueDev <lhdeveloper137@gmail.com>
2025-08-18 10:02:26 -0300

    :sparkles: rectangle example

--- main.cpp
+++ main.cpp
@@ -12,6 +12,113 @@ static void processInput(GLFWwindow* window) {
         glfwSetWindowShouldClose(window, true);
 }
 
+class RectangleCore {
+public:
+    RectangleCore() {
+        float vertices[] = {
+             0.5f,  0.5f, 0.0f,  // top right
+             0.5f, -0.5f, 0.0f,  // bottom right
+            -0.5f, -0.5f, 0.0f,  // bottom left
+            -0.5f,  0.5f, 0.0f   // top left 
+        };
+
+        unsigned int indices[] = {  // note that we start from 0!
+            0, 1, 3,   // first triangle
+            1, 2, 3    // second triangle
+        };
+
+        glGenBuffers(1, &this->EBO);
+
+
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+        // 1. Criar e vincular o VAO
+        glGenVertexArrays(1, &VAO);
+        glBindVertexArray(VAO);
+
+        // 2. Criar e vincular o VBO
+        glGenBuffers(1, &VBO);
+        glBindBuffer(GL_ARRAY_BUFFER, VBO);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+        
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, this->EBO);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+        // 3. Compilar shaders
+        const char* vertexShaderSrc = R"(
+                #version 330 core
+                layout (location = 0) in vec3 aPos;
+                void main() { gl_Position = vec4(aPos, 1.0); }
+            )";
+        const char* fragmentShaderSrc = R"(
+                #version 330 core
+                out vec4 FragColor;
+                void main() { FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f); }
+            )";
+
+        unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+        glShaderSource(vertexShader, 1, &vertexShaderSrc, NULL);
+        glCompileShader(vertexShader);
+        checkCompileErrors(vertexShader, "VERTEX");
+
+        unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+        glShaderSource(fragmentShader, 1, &fragmentShaderSrc, NULL);
+        glCompileShader(fragmentShader);
+        checkCompileErrors(fragmentShader, "FRAGMENT");
+
+        shaderProgram = glCreateProgram();
+        glAttachShader(shaderProgram, vertexShader);
+        glAttachShader(shaderProgram, fragmentShader);
+        glLinkProgram(shaderProgram);
+        checkCompileErrors(shaderProgram, "PROGRAM");
+        glDeleteShader(vertexShader);
+        glDeleteShader(fragmentShader);
+
+
+
+        // 4. Configurar layout de vértice **após vincular VAO**
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+        glEnableVertexAttribArray(0);
+
+        // 5. Desvincular (opcional, para limpagem)
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
+        glBindVertexArray(0);
+    }
+
+    void Draw() const {
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO);
+        glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        glBindVertexArray(0);
+    }
+
+private:
+    unsigned int VBO = 0, VAO = 0, EBO = 0, shaderProgram = 0;
+
+    void checkCompileErrors(unsigned int shader, const string& type) {
+        int success;
+        char infoLog[1024];
+        if (type != "PROGRAM") {
+            glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+            if (!success) {
+                glGetShaderInfoLog(shader, 1024, NULL, infoLog);
+                cout << "ERROR::SHADER_COMPILATION_ERROR of type: " << type << "\n"
+                    << infoLog << "\n -- --------------------------------------------------- -- \n";
+            }
+        }
+        else {
+            glGetProgramiv(shader, GL_LINK_STATUS, &success);
+            if (!success) {
+                glGetProgramInfoLog(shader, 1024, NULL, infoLog);
+                cout << "ERROR::PROGRAM_LINKING_ERROR of type: " << type << "\n"
+                    << infoLog << "\n -- --------------------------------------------------- -- \n";
+            }
+        }
+    }
+};
+
 class GameGraphicsCore {
 public:
     GameGraphicsCore() {
@@ -122,7 +229,7 @@ int main() {
     glViewport(0, 0, 800, 600);
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
-    GameGraphicsCore core;
+    RectangleCore core;
 
     while (!glfwWindowShouldClose(window)) {
         processInput(window);
